8b387d77eae440a85052bc3d89952a5f167e4d10
Saulo Santiago <saulodasilvasantiago@gmail.com>
2012-03-23 02:52:19 -0300

    fazendo um exemplo de cadastro no form ja existente

--- web_app/app/controllers/users_controller.rb
+++ web_app/app/controllers/users_controller.rb
@@ -3,9 +3,13 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     
     if @user.save
-      
+      redirect_to users_path
     else
-      
+      render :action => 'user'
     end
   end
+
+  def index
+    @users = User.all
+  end 
 end
--- web_app/app/models/address.rb
+++ web_app/app/models/address.rb
@@ -1,10 +1,12 @@
 class Address
   include Mongoid::Document
   include Mongoid::Timestamps
+  has_many :user
   
   field :number, :type => String
   field :address, :type => String
   field :neighborhood, :type => String
   field :city, :type => String
   field :postal_code, :type => String
+  field :state, :type => String
 end
