4e3564a4e5c8cde528434e7bbf7e92602f5d25a1
Saulo Santiago <saulodasilvasantiago@gmail.com>
2012-03-26 02:12:22 -0300

    colocando um helper para os form aninhados

--- web_app/app/controllers/pages_controller.rb
+++ web_app/app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
   def index
     @person = Person.new
-    @person.build_address if @person.address blank?
-    @person.build_user
-    @person.build_contact if @person.contact blank?
   end 
 
 end
--- web_app/app/helpers/people_helper.rb
+++ web_app/app/helpers/people_helper.rb
@@ -1,6 +1,6 @@
 module PeopleHelper
-  def setup_test(test)
-    test.tap do |r|
+  def setup_person(person)
+    person.tap do |r|
       r.build_address if r.address.blank?
       r.build_user if r.user.blank?
       r.build_contact if r.contact.blank?
