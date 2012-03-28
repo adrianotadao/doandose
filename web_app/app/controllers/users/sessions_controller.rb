8af7947e24a78c4a0a166f8fbd82828904764e09
Saulo Santiago <saulodasilvasantiago@gmail.com>
2012-03-28 02:10:12 -0300

    arrumando o modelo do user o controller do application e do session arrumando o initializer do ominiauth

--- web_app/app/controllers/users/sessions_controller.rb
+++ web_app/app/controllers/users/sessions_controller.rb
@@ -1,24 +1,13 @@
 class Users::SessionsController < ApplicationController  
-  def new  
-    if user_signed_in?
-      redirect_to edit_user_path 
-    else
-      @user = User.new
-    end
-  end  
-  
-  def create  
-    user = User.from_omniauth(env["omniauth.auth"])  
-    session[:user_id] = user.id  
-    redirect_to root_url, notice: "Signed in!"  
-  end  
-  
-  def destroy  
-    session[:user_id] = nil  
-    redirect_to root_url, notice: "Signed out!"  
-  end  
-  
-  def failure  
-    redirect_to root_url, alert: "Authentication failed, please try again."  
-  end  
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Signed out!"
+  end
 end
