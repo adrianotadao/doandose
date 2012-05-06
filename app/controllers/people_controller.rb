f0b2917d8fb2516d3e87111d0690ebf212d18565
Saulo Santiago <saulodasilvasantiago@gmail.com>
2012-05-06 02:50:13 -0300

    inserindo step form no cadastro do doador

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,7 +1,8 @@
 class PeopleController < ApplicationController
 
   def new
-    @person = Person.new
+    session[:person_params] ||= {}
+    restore_session
   end 
 
   def show
@@ -9,14 +10,30 @@ class PeopleController < ApplicationController
   end
 
   def create
-    @person = Person.new(params[:person])
-    @person.blood_id = 1    
-    
-    if @person.save
-      redirect_to root_path
+    session[:person_params].deep_merge!(params[:person]) if params[:person]
+    restore_session
+
+    if params[:back_button]
+      @person.previous_step
+    elsif @person.last_step?
+      @person.blood_id = 1 
+      @person.save 
     else
-      render :action => 'new'
-    end    
+      @person.next_step
+    end
+    session[:person_step] = @person.current_step
+
+    if @person.new_record?
+      render 'new'
+    else
+      flash[:notice] = 'cadastro efetuado com sucesso'
+      redirect_to root_path
+    end  
   end
     
+  private
+    def restore_session
+      @person = Person.new(session[:person_params])
+      @person.current_step = session[:person_step]
+    end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -32,4 +32,30 @@ class Person
                   :donor, :surname, :sex, :weight, :height, :birthday, :observations, :blood, :email, :phone, 
                   :cellphone, :zip_code, :street, :number, :neighborhood, :city, :state, :provider, :uid, :email, 
                   :username
+  attr_writer :current_step
+
+  #others
+  def current_step
+    @current_step || steps.first
+  end
+  
+  def steps
+    %w[user information]  
+  end
+
+  def next_step
+    self.current_step = steps[ steps.index(current_step) + 1]
+  end
+
+  def previous_step
+    self.current_step = steps[ steps.index(current_step) - 1]
+  end
+
+  def first_step?
+    current_step == steps.first  
+  end
+
+  def last_step?
+    current_step == steps.last  
+  end
 end
