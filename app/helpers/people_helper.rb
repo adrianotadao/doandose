2448159f323fd5d1f3efad4c5fc1cf2c3115a477
Adriano Tadao <drianotadao@gmail.com>
2012-06-26 02:05:10 -0300

    Corrigindo processo do cadastro. Está incorreto o processo de plotagem.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -2,4 +2,13 @@ module PeopleHelper
   def path_locale(attribute)
     t("label.user.confirmation_step.#{attribute}")
   end
+  
+  def people_markers
+    markers = []
+    Person.actives.all.map(&:address).each do |address|
+      markers << address.full_coordinate if address.full_coordinate
+    end
+    markers   
+  end
+  
 end
