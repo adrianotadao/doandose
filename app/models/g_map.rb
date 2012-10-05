1570d0513a6c0d92a23b5a365eb0122d11ab09f6
Adriano Tadao <drianotadao@gmail.com>
2012-10-05 01:18:48 -0300

    Mongod geospacial

--- app/models/g_map.rb
+++ app/models/g_map.rb
@@ -1,26 +1,8 @@
 class GMap
   class << self
-    def initializer
-    end
-
-    #number street_name, neighborhood, City - State
-    def coordinates(address)
-      Geokit::Geocoders::GoogleGeocoder.geocode(address)
-    end
-
-    def user_booble(user)
-    end
-
-    def user_route(x_point, y_point)
-    end
-
-    def users_in_booble(users)
-    end
-
-    def users_in(distance)
-    end
-
-    def address_by_postal_code(postal_code)
+    def elements_by_distance(point, distance, type)
+      earthRadius = 6371 #km
+      Address.where(:addressable_type => type, :loc => {"$within" => {"$centerSphere" => [point, (distance.fdiv(earthRadius) )]}})
     end
   end
 end
