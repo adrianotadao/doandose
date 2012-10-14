58499205b0291ef1fc1bb57099b4b3f5569a973b
Saulo da Silva <saulodasilvasantiago@gmail.com>
2012-10-14 16:59:08 -0300

    inserindo redes sociais

--- app/helpers/social_networks_helper.rb
+++ app/helpers/social_networks_helper.rb
@@ -3,14 +3,14 @@ module SocialNetworksHelper
     content_for :javascript do
       "<div id='fb-root'></div>".html_safe
     end
+
     add_javascript 'http://connect.facebook.net/pt_BR/all.js#xfbml=true', { :async => true }
 
     unless options[:no_override]
       url = 'http://www.facebook.com/doando.se'
-
     end
-    default_options = { :href => url, :layout => 'button_count', :show_faces => false, :class => 'facebook', :font => 'tahoma' }
 
+    default_options = { :href => url, :layout => 'button_count', :show_faces => false, :class => 'facebook', :font => 'tahoma' }
     haml_tag 'fb:like', default_options.merge(options)
   end
 
@@ -20,7 +20,7 @@ module SocialNetworksHelper
 
   def twitter_like(url, text, options={})
     add_javascript 'http://platform.twitter.com/widgets.js', { :async => true }
-    haml_tag :a, :href => "http://twitter.com/share", :class => 'twitter-share-button', 'data-related' => 'clickjogos',
+    haml_tag :a, :href => "http://twitter.com/share", :class => 'twitter-share-button', 'data-related' => 'doando.se',
       'data-count' => 'horizontal', 'data-counturl' => url, 'data-text' => text,
       'data-hashtags' => options[:hash_tags].to_a.join(',').downcase do
         haml_concat 'Tweet'
