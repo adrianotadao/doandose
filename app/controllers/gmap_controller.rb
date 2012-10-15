107743b72969a6c7a242dd25746d8b603b91c670
Adriano Tadao <drianotadao@gmail.com>
2012-10-15 02:17:54 -0300

    Melhorando controller

--- app/controllers/gmap_controller.rb
+++ app/controllers/gmap_controller.rb
@@ -1,7 +1,9 @@
 class GmapController < ApplicationController
   def elements_by_user_position
-    @companies = GMap.elements_by_distance([params[:positions][:lat].to_f, params[:positions][:lng].to_f], 5, 'Company').map(&:addressable).map{|r| [r.address.loc[0], r.address.loc[1]]}
-    @people = GMap.elements_by_distance([params[:positions][:lat].to_f, params[:positions][:lng].to_f], 5, 'Person').map(&:addressable).map{|r| [r.address.loc[0], r.address.loc[1]]}
+    position = [params[:positions][:lat].to_f, params[:positions][:lng].to_f]
+
+    @companies = GMap.elements_by_distance(position, 5, 'Company').map(&:addressable).map{|r| [r.address.loc[0], r.address.loc[1]]}
+    @people = GMap.elements_by_distance(position, 5, 'Person').map(&:addressable).map{|r| [r.address.loc[0], r.address.loc[1]]}
 
     render json: { people: @people, companies: @companies}
   end
