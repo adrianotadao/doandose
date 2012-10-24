671b275f1408c532a69d6f3fb286ff3223c7fb27
Saulo da Silva <saulodasilvasantiago@gmail.com>
2012-10-24 00:51:52 -0200

    removendo outros requires desnecessarios

--- app/assets/javascripts/users/validation/validations.js
+++ app/assets/javascripts/users/validation/validations.js
@@ -6,14 +6,4 @@
  *= require ./weight_validation
  *= require ./height_validation
 
- *= require ./auth_key_validation
-
- *= require ./parents_email
- *= require ./terms_of_service_validation
- *= require ./current_password_validation
- *= require ./email_validation
- *= require ./gender_validation
- *= require ./password_confirmation_validation
- *= require ./password_validation
-
  */
