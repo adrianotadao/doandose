edcaf29bf38a3add1c91e2ac315d4b624dff5db1
Adriano Tadao <drianotadao@gmail.com>
2012-11-04 22:44:11 -0200

    fazendo pull

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -6,22 +6,19 @@ describe 'Notification' do
   end
 
   it 'Should have a quantity' do
-    FactoryGirl.build(:notification, :quantity => nil).should_not be_valid
+    FactoryGirl.build(:notification, quantity: nil).should_not be_valid
   end
 
   it 'Should have a situation' do
-    FactoryGirl.build(:notification, :situation => nil).should_not be_valid
+    FactoryGirl.build(:notification, situation: nil).should_not be_valid
   end
 
   it 'Should have a company' do
-    FactoryGirl.build(:notification, :company => nil).should_not be_valid
+    FactoryGirl.build(:notification, company: nil).should_not be_valid
   end
 
   it 'Should have a blood' do
-    FactoryGirl.build(:notification, :blood => nil).should_not be_valid
+    FactoryGirl.build(:notification, blood: nil).should_not be_valid
   end
 
-  it 'Should have a alert date' do
-    FactoryGirl.build(:notification, :alerted_at => nil).should_not be_valid
-  end
 end
