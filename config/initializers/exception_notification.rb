626a6584d2a4f4d42568ed6ff7af2dd03f0da423
Adriano Tadao <drianotadao@gmail.com>
2012-12-01 00:28:32 -0200

    adicionado exception notification

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,5 +1,5 @@
 if defined?(ExceptionNotifier)
-  ClickJogos::Application.config.middleware.use ExceptionNotifier,
+  Doandose::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => '[EXCEPTION]',
     :sender_address => 'suporte@doando.se',
     :exception_recipients => 'exceptions@doando.se'
